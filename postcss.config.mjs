fb8e23e517bd2a5856799f78391341de99e7409a
Ssdsai <steve161201@gmail.com>
2025-03-04 12:55:12 -0600

    Updates

--- postcss.config.mjs
+++
@@ -1,9 +0,0 @@
-// Assign the object to a variable first
-const config = {
-  plugins: {
-    autoprefixer: {}, // Automatically add vendor prefixes to CSS rules
-  },
-};
-
-// Export the config object as default
-export default config;
